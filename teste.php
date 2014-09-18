0848caebc8814d9d425065bccb1a969cae242d94
31437771 <31437771@087885-P33S105.macklabs.br>
2014-09-18 11:46:46 -0300

    correções

--- teste.php
+++ teste.php
@@ -5,10 +5,16 @@
 	<meta charset="utf-8">
 </head>
 <body>
+    
+    X = 2
+    Y = 3
 
 <?php
-include "operacoes.php"
-
+include "operacoes.php";
+echo soma(2, 3);
+echo sub(2, 3);
+echo mult(2, 3);
+echo div(2, 3);
 ?>
 
 </body>
